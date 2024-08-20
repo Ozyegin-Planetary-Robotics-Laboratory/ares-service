71b658d7ca7b6c7d477229cef96e666040eacb73
Toprak <efe.akkilic@ozu.edu.tr>
2024-08-20 18:55:45 +0300

    Separated into two folders and rewrote the build system.

--- scripts/canable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo ip link set can0 down
-sudo ip link set can0 type can bitrate 500000
-sudo ip link set can0 up
--- scripts/env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export ROS_MASTER_URI=http://ares:11311
-export ROS_HOSTNAME=ares
